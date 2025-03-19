ea7f1f63c3023dfcd787d93391a136e20e976b40
Abhishek Upadhyay <abhishekupadhyay0007@gmail.com>
2025-03-19 13:12:12 +0530

    user ui check

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,7 @@ import UserLogout from './pages/UserLogout'
 import CaptainHome from './pages/CaptainHome'
 import CaptainProtectWrapper from './pages/CaptainProtectWrapper'
 import CaptainLogout from './pages/CaptainLogout'
+import Riding from './pages/Riding'
 
 const App = () => {
   return (
@@ -43,6 +44,8 @@ const App = () => {
             <CaptainLogout />
           </CaptainProtectWrapper>
           } />
+
+        <Route path="/riding" element={<Riding />} />
 
       </Routes>
     </div>
